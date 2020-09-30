68e7cf3e620add034c49a9fb2c1e87c31bc3e6f4
Ankit Podder <50513363+ANKITPODDER2000@users.noreply.github.com>
2020-09-30 16:38:05 +0530

    Update model.py
    If we are using tensorflow , then we have to normalize the data , 
    x = x - mean / std
    y = y - mean / std
    if we don't give normalize data to the model , then our model don't work properly , so it is very important to normalize the data .

--- model.py
+++ model.py
@@ -5,13 +5,29 @@ import matplotlib.pyplot as plt
 x = np.array([1,2,3,4,5,6,7,9])
 y = x*2+10
 
+mean_x = x.mean()
+x = x - mean_x
+std_x = x.std()
+x = x / std_x
+
+mean_y = y.mean()
+y = y - mean_y
+std_y = y.std()
+y = y / std_y
+
 print("x is : ",x)
 print("y is : ",y)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.Dense(1 , input_shape = [1,])
 ])
-model.compile()
-model.fit(x,y,epochs=10)
+model.summary()
+model.compile(optimizer = "rmsprop" , loss = "mae")
+his = model.fit(x,y,epochs=10)
+
+import matplotlib.pyplot as plt
+plt.plot(his.history['loss'])
+plt.title("Loss Curve")
+plt.plot()
 
 print("Prediction : ",(model.predict((np.array([10,20,30,80])-mean_x)/std_x) * std_y)+mean_y)
